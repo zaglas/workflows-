095dbe950f6453652a8c87010f757002139eb41d
zaglas <58752077+zaglas@users.noreply.github.com>
2024-01-02 19:54:31 +0200

    Update script-to-upload.py

--- script-to-upload.py
+++ script-to-upload.py
@@ -1,24 +1,22 @@
 import os
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
 def main():
-    creds = Credentials(None,
-                        refresh_token=os.environ['REFRESH_TOKEN'],
-                        token_uri='https://oauth2.googleapis.com/token',
-                        client_id=os.environ['CLIENT_ID'],
-                        client_secret=os.environ['CLIENT_SECRET'])
+    client_id = os.environ['CLIENT_ID']
+    client_secret = os.environ['CLIENT_SECRET']
+    refresh_token = os.environ['REFRESH_TOKEN']
+    file_path = os.environ['FILE_PATH']
 
-    service = build('drive', 'v3', credentials=creds)
+    credentials = Credentials(None, refresh_token=refresh_token, token_uri='https://oauth2.googleapis.com/token', client_id=client_id, client_secret=client_secret)
+    service = build('drive', 'v3', credentials=credentials)
 
-    file_metadata = {'name': 'myfile.txt'}
-    media = MediaFileUpload(os.environ['FILE_PATH'],
-                            mimetype='text/plain')
-
-    file = service.files().create(body=file_metadata,
-                                  media_body=media,
-                                  fields='id').execute()
-    print('File ID: %s' % file.get('id'))
+    file_metadata = {'name': os.path.basename(file_path)}
+    media = MediaFileUpload(file_path, mimetype='application/octet-stream')
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(f"File ID: {file.get('id')}")
 
 if __name__ == '__main__':
     main()
+
