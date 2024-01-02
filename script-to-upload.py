879759cadad161ffd798e921cd9eba4c1f95a856
zaglas <58752077+zaglas@users.noreply.github.com>
2024-01-02 20:30:37 +0200

    Update script-to-upload2.py

--- script-to-upload.py
+++ script-to-upload.py
@@ -8,11 +8,15 @@ def main():
     client_secret = os.environ['CLIENT_SECRET']
     refresh_token = os.environ['REFRESH_TOKEN']
     file_path = 'test.txt' 
-
+    folder_id = '1NTyoZ7m72E-7Pw3DWmXbDyj9qnjdXiS5' 
+    
     credentials = Credentials(None, refresh_token=refresh_token, token_uri='https://oauth2.googleapis.com/token', client_id=client_id, client_secret=client_secret)
     service = build('drive', 'v3', credentials=credentials)
 
-    file_metadata = {'name': os.path.basename(file_path)}
+    file_metadata = {
+        'name': os.path.basename(file_path),
+        'parents': [folder_id]  # Specify the folder ID
+    }
     media = MediaFileUpload(file_path, mimetype='application/octet-stream')
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     print(f"File ID: {file.get('id')}")
